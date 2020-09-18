f1a33926a3b49d20dee8e4dc08eaf3f182d88a9b
Zaph-x <mikkel@luceartfoto.dk>
2020-09-18 13:12:03 +0200

    Fixed bug

--- Core/Macros/TextComponent.cs
+++ Core/Macros/TextComponent.cs
@@ -1,5 +1,8 @@
-﻿namespace Core.Macros {
-    public class TextComponent {
-        
+﻿namespace Core.Macros
+{
+    public abstract class TextComponent
+    {
+        public string Result { get; set; }
+        public abstract string WriteComponent();
     }
 }
